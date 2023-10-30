b743a41f9c4670a99804b1405607b817bb5495b3
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-10-30 01:06:46 -0500

    add shuffle logic with Fisher-Yates and impletement useEffect with score board; add basic css

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import './App.css'
 
 function App() {
   const [cards, setCards] = useState([])
-  
-
+  const [clickedCards, setClickedCards] = useState(new Set())
+  const [currScore, setCurrScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
   
 
   async function getCards( index) {
@@ -35,17 +36,78 @@ function App() {
     )
   },[])
 
+  //compare the updated current score to best score and update
+  useEffect(() =>{
+    if (bestScore < currScore) {
+      setBestScore(currScore)
+    }
+  },[currScore, bestScore])
+  //use Fisher-Yates shuffle (basically walk the array in reverse order and swap each element with a random one)
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      //find a random index from 0 to i
+      // let random = Math.random();
+      // let oneMore = i + 1;
+      // let oneMoreTimesRandom = oneMore * random;
+      // let jfloor = Math.floor(oneMoreTimesRandom);
+      // let j = jfloor;
+      let j = Math.floor(Math.random() * (i + 1));
+      // let iVal = array[i];
+      // let jVal = array[j];
+      // array[i] = jVal;
+      // array[j] = iVal;
+      // let temp = array[i];
+      // array[i] = array[j];
+      // array[j] = temp;
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+  }
+
+  function handleClick(e) {
+    //if the game is over, new clicks wont do anything
+    const gameResult = document.querySelector('.gameResult')
+    if (clickedCards.size === 12 || gameResult.innerHTML === 'Game over!') {
+      return;
+    }
+    //check if this card has been clicked before and update score and best score
+    if (!clickedCards.has(e.target.className)) {
+      gameResult.innerHTML = ''
+      clickedCards.add(e.target.className)
+      if (clickedCards.size === 12) {
+        gameResult.innerHTML = 'You win!'
+      } 
+      console.log(cards.filter(card => !clickedCards.has(card.name)).map(card => card.name))
+      setCurrScore(currScore => currScore + 1)
+    } else {
+      gameResult.innerHTML = 'Game Over!'
+    }
+    setCards(()=> {
+      const newCards = JSON.parse(JSON.stringify(cards));
+      //shuffle the image array 
+      shuffle(newCards);
+      return newCards;
+    })
+  }
   
-  
+  function handleNewRound() {
+    setCurrScore(0)
+    setClickedCards(new Set())
+  }
+   
 
   return (
     <>
       <div className='gameInfo'>
-        <div className='gameTitle'>Pokeman Memory Card</div>
-        <div className='gameResult'></div>
-        <div className='scoreBoard'>
-          <div className='currScore'>Score: </div>
-          <div className='bestScore'>Best Score: </div>
+        <div className='gameTitleAndScore'>
+          <div className='gameTitle'>Pokeman Memory Card</div>
+          <div className='scoreBoard'>
+            <div className='currScore'>Score: {currScore}</div>
+            <div className='bestScore'>Best Score: {bestScore}</div>
+          </div>
+        </div>
+        <div className='gameResultAndButton'>
+          <div className='gameResult'>Start clicking! Just do not click the same Pokeman twice!</div>
+          <button className='newGame' onClick={handleNewRound}>New Round</button>
         </div>
       </div>
       <div className='allCardsContainer'>
@@ -53,7 +115,7 @@ function App() {
           cards.map(card => {
             return (
               <div className='cardInfo' key={card.id} >
-                <img src= {card.url} alt="pokeman Picture" />
+                <img src= {card.url} alt="pokeman Picture" className= {card.name} onClick={handleClick}/>
                 <div className='pokemanName'>{card.name}</div>
               </div>
             )
